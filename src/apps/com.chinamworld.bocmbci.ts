d83f93c8616908ae40174a5dfb7b1f8fc527da47
Ling0402 <cjy081219@Gmail.com>
2026-04-11 18:01:18 +0800

    feat: 中国银行 全屏广告-弹窗广告_新形式 (#114)
    * feat: 中国银行 全屏广告-弹窗广告
    
    * perf: key: 2
    
    * perf: 优化
    
    ---------
    
    Co-authored-by: 天方夜谭 <tianfangyetan01@qq.com>

--- src/apps/com.chinamworld.bocmbci.ts
+++ src/apps/com.chinamworld.bocmbci.ts
@@ -31,6 +31,15 @@ export default defineGkdApp({
           exampleUrls: 'https://e.gkd.li/13b6fa95-5198-41f5-b39c-d40166a624fb',
           snapshotUrls: 'https://i.gkd.li/i/19567854',
         },
+        {
+          key: 2,
+          activityIds:
+            'com.boc.bocsoft.mobile.bocmobile.buss.system.main.ui.MainActivity',
+          matches:
+            '@Image[width<80 && height<80] <2 View[childCount=2] <<2 View[index=parent.childCount.minus(1)] <n [id="app"] <<4 [vid="webview_layout"]',
+          exampleUrls: 'https://e.gkd.li/98cd7f08-a225-4fcc-8c4f-bd92e1359bda',
+          snapshotUrls: 'https://i.gkd.li/i/26644706',
+        },
       ],
     },
   ],
